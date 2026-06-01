57b779d5b833355dd4d74398e1cc66d639a6fdac
Anshika bajpai <iamsomyabajpai@gmail.com>
2026-06-01 21:38:57 +0100

    Initial commit

--- src/patternPrinting/pattern9.java
+++ src/patternPrinting/pattern9.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class pattern9 {
      public static void main() {
-         //Scanner sc= new Scanner(System.in);
-         //int n = sc.nextInt();
-         int n=5;
+         Scanner sc= new Scanner(System.in);
+         int n = sc.nextInt();
+
          for (int i =1; i<=n;i++){
             if (i<n) {
                 for (int j = 1; j <= i; j++) {
