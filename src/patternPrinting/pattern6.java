6bd11f9c7716629fd43ad44f7c0aa39964b2f502
Anshika bajpai <iamsomyabajpai@gmail.com>
2026-06-02 22:49:44 +0100

    Initial commit

--- src/patternPrinting/pattern6.java
+++ src/patternPrinting/pattern6.java
@@ -18,6 +18,7 @@ public class pattern6 {
                 }
                 System.out.println();
 
+
             }
         }
     }
---
+++ src/patternPrinting/pattern7.java
@@ -0,0 +1,15 @@
+package patternPrinting;
+
+public class pattern7 {
+    static void main() {
+       int n = 4;
+       for(int i =1; i <=n;i++) {
+
+           for(int j =1; j<=i-1;i++){
+               System.out.print(" ");
+           }
+           System.out.println();
+           for (int k = 1; k<=2;k++);
+       }
+    }
+}
