a94178f44c00081a897354c5a573ae76fb52f83b
Anshika bajpai <iamsomyabajpai@gmail.com>
2026-06-02 22:49:28 +0100

    Initial commit

---
+++ src/patternPrinting/pattern10.java
@@ -0,0 +1,13 @@
+package patternPrinting;
+
+import java.util.Scanner;
+
+public class pattern10 {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for (int i = 1; i<=n;i++){
+
+        }
+    }
+}
