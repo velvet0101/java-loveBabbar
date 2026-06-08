907e23395a567228b1528115e2ba613a3778af45
Anshika bajpai <iamsomyabajpai@gmail.com>
2026-06-08 22:41:16 +0100

    Initial commit

--- src/patternPrinting/pattern10.java
+++ src/patternPrinting/pattern10.java
@@ -6,7 +6,18 @@ public class pattern10 {
     public static void main() {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
+        //incomplete
         for (int i = 1; i<=n;i++){
+            for(int j =1; j<=n-i;j++){
+                System.out.print("  ");
+            }
+            System.out.println();
+            for (int k=1;k<=2*i-1;i++){
+                if (k==1||k==n){
+
+                    System.out.print("* ");
+                }
+            }
 
         }
     }
--- src/patternPrinting/pattern7.java
+++ src/patternPrinting/pattern7.java
@@ -3,6 +3,7 @@ package patternPrinting;
 public class pattern7 {
     static void main() {
        int n = 4;
+       //incomplete
        for(int i =1; i <=n;i++) {
 
            for(int j =1; j<=i-1;i++){
